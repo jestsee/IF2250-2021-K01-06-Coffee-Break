33f8496b50eb4ea89abaf07cce8967f040d6bee1
eruds <erutan.ds20@gmail.com>
2021-04-19 16:48:58 +0700

    I don't know what i'm doing

--- activity_page.py
+++ activity_page.py
@@ -80,7 +80,28 @@ class ActivityPage(tk.Frame):
         add_act_button.config(width=67, height=4)
         add_act_button.place(x=715, y=500)
 
+class Application(tk.Tk):
+    def __init__(self,*args,**kwargs):
+        tk.Tk.__init__(self,*args,**kwargs)
 
+        #Creating a window
+        window = tk.Frame(self)
+        window.pack()
+
+        window.grid_rowconfigure(0, minsize = 720)
+        window.grid_columnconfigure(0, minsize = 1280)
+
+        self.frames = {}
+
+        frame = ActivityPage(window, self)
+        self.frames[ActivityPage] = frame
+        frame.grid(row = 0, column = 0, sticky="nsew")        
+
+        self.show_frame(ActivityPage)
+
+    def show_frame(self, page):
+        frame = self.frames[page]
+        frame.tkraise()
 
 #Database
 #Create or connect database activity
