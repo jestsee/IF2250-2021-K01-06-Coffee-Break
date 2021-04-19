d85190bc68e6365ecdd6d0b7fa5d088c08fc89bd
eruds <erutan.ds20@gmail.com>
2021-04-19 17:26:17 +0700

    Testing flow merge

--- Frontend/Datatypes/Chat.py
+++ Frontend/Datatypes/Chat.py
@@ -13,13 +13,13 @@ class Chat :
         if(type(chatId) != int or type(firstUserId) != int or type(secondUserId) != int) :
             raise ValueError("Invalid input for initiating object of type chat")
         # Values 
-        if(type(messages) != list) : 
-            if(len(messages) > 0 and type(messages[0]) != Message) : 
-                raise ValueError("Invalid input for initiating object of type chat; messages parameter must be a list of object Messages")
+        if(type(messages) != list or ( len(messages) > 0 and type(messages[0]) != Message )) : 
+            raise ValueError("Invalid input for initiating object of type chat; messages parameter must be a list of object Messages")
         self.__chatId = chatId
         self.__firstUserId = firstUserId
         self.__secondUserId = secondUserId
         self.__messages = messages
+        
     
     def printMessages(self) : 
         for message in self.__messages : 
