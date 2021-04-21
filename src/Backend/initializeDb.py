ae2dea5c3663872cf932f66f6d98a3080c989cb3
eruds <erutan.ds20@gmail.com>
2021-04-21 17:57:48 +0700

    Added DeleteAllMessageHistory Button

--- src/Backend/initializeDb.py
+++ src/Backend/initializeDb.py
@@ -9,7 +9,22 @@ conn = sqlite3.connect(dir_path + '/storage.db')
 #Create cursor
 c = conn.cursor()
 
-#Create Activity Table 
+# Create User Table
+# Create table buat nampung data user
+c.execute('''CREATE TABLE IF NOT EXISTS user
+             (user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+             nama TEXT, 
+             dom TEXT, 
+             hobi TEXT)''')
+
+# Create table buat nampung data teman yg fk nya references ke table user
+c.execute('''CREATE TABLE IF NOT EXISTS teman
+             (user_id1 INTEGER, user_id2 INTEGER,
+                PRIMARY KEY (user_id1, user_id2),
+                FOREIGN KEY (user_id1) REFERENCES user (user_id),
+                FOREIGN KEY (user_id2) REFERENCES user (user_id))''')
+
+# Create Activity Table 
 c.execute("""
 CREATE TABLE IF NOT EXISTS activity (
     activityId integer PRIMARY KEY,
@@ -40,7 +55,9 @@ c.execute("""CREATE TABLE IF NOT EXISTS message(
         content VARCHAR(255),
         timestamp DATETIME NOT NULL,
         chatroomId integer NOT NULL,
+        senderId integer NOT NULL,
         FOREIGN KEY (chatroomId) REFERENCES chatroom(chatroomId)
+        FOREIGN KEY (senderId) REFERENCES user(user_id)
 ) """)
 
 #Commit changes
